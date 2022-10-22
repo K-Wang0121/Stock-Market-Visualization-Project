0e74ac72d969077253280429a16568ae6c4ea09d
K-Wang0121 <113778291+K-Wang0121@users.noreply.github.com>
2022-10-22 20:36:40 +0800

    Update DataQuest_Notes.py

--- DataQuest_Notes.py
+++ DataQuest_Notes.py
@@ -57,7 +57,7 @@ with st.sidebar:
 		options = ['Abstract', 'Background Information', 'Data Cleaning', 
 		'Data Analysis', 'Conclusion', 'Bibliography'],
 		menu_icon = 'menu-up',
-		icons = ['bookmark-check', 'book', 'clipboard2-check', 'map', 'boxes', 'bar-chart', 
+		icons = ['bookmark-check', 'book', 'clipboard2-check', 'map', 'boxes', 
 		'bookmarks'],
 		default_index = 0
 		)
